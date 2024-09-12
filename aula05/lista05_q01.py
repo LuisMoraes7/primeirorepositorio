3401fdcf8d3510902a076551e66f0c0940d1b976
LuisMoraes7 <luisedunascimento17@gmail.com>
2024-09-11 21:39:29 -0300

    att

--- aula05/lista05_q01.py
+++ aula05/lista05_q01.py
@@ -1,10 +1,14 @@
 
 
 def resolucao():
+    print('''Um mapa do tesouro tem duas partes, A e B. A primeira parte está escondida no X
+número de passos para o norte, e a segunda no Y número de passos para o leste. Crie um
+programa que receba os valores de X e Y e calcule a distância total que o pirata precisa
+percorrer para chegar ao tesouro (soma de X e Y).''')
     x = int(input('Digite quantos passos são necessários para chegar na parte A do mapa: '))
     y = int(input('Digite quantos passos são necessários para chegar na parte B do mapa: '))
     total = x + y
-    return (print(f'O total de passos é {total} ({x} + {y}).'))
+    return (print(f'O total de passos é {total} ({x} na parte A + {y} na parte B).'))
 
 
 
---
+++ aula05/lista05_q02.py
@@ -0,0 +1,22 @@
+
+
+def resolucao():
+    print('''Descrição: Um guerreiro precisa de uma armadura especial para a batalha. Para forjar a
+armadura, ele precisa de uma liga metálica que combina ferro e ouro. O ferreiro diz que a liga
+precisa ter no mínimo 70% de ferro. Crie um programa que receba a quantidade de ferro e
+ouro em kg e verifique se a porcentagem de ferro na liga é suficiente.''')
+    ferro = int(input('Digite a quantidade de ferro em kg: '))
+    ouro = int(input('Digite a quantidade de ouro em kg: '))
+    total = ferro + ouro
+    if (ferro >= 0.7 * total):
+        return(print('A porcentagem do ferro é maior do que 70% do total.'))
+    else:
+        return(print('A porcentagem do ferro não corresponde nem a 70% do total.'))
+
+
+if __name__ == '__main__':
+    print("----------- Iniciando o programa ------------")
+
+    resolucao()
+
+    print("------------- Fim do programa ---------------")
